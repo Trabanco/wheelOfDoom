bbc2418b6f1e64ab977fab44189cde916034b222
Mario Rubio <marioastur@gmail.com>
2022-06-14 00:22:39 +0200

    JS - Añade a la lista y a las cajas

--- js/app.js
+++ js/app.js
@@ -3,10 +3,40 @@ const editButton = document.getElementById('edit')
 const deleteButton = document.getElementById('delete')
 const playButton = document.getElementById('play')
 
+let playersListArray = []
+
 
 addButton.onclick = function() {
-    console.log('Add jugador a la lista')
+    const player = document.getElementById('input')
+    if (player.value.trim() !== '') {
+        addPlayerToList(player.value)
+        addPlayerToBox(player.value)
+    }
+    player.value = ''
+    player.focus()
 }
+
+function addPlayerToList(player) {
+    buttonsOnList = '<button type="button" class="buttonlist" id="edit"><img src="./src/editButton.png"></button><button type="button" class="buttonlist" id="delete"><img src="./src/deleteButton.png"></button>'
+    let listPlayers = document.getElementById('orderlist')
+    player = player + buttonsOnList
+    listPlayers.appendChild(document.createElement('li')).innerHTML = player
+    playersListArray.push(player)
+}
+
+function addPlayerToBox(player) {
+    const boxOfPlayers = document.querySelector('.boxes')
+    const newBox = document.createElement('div')
+    newBox.className = 'box'
+    boxOfPlayers.appendChild(newBox).textContent = player
+}
+
+
+
+
+
+
+
 
 editButton.onclick = function() {
     console.log('Editar jugador de la lista')
