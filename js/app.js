40215a930636cb82e7c086d6a94a40ec58676287
Mario Rubio <marioastur@gmail.com>
2022-06-14 21:58:47 +0200

    Colores aleatorios fonde de cajas

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ function app() {
     const editButton = document.getElementById('edit')
     const deleteButton = document.getElementById('delete')
     const playButton = document.getElementById('play')
+    const colors = ['D93C51', 'F6EB15', '4893D8', 'A642AD', '4C6BDB', '186887', 'DF3C54', 'ED8EF7', 'F6EB15', 'F98323', 'ED8EF7', '52ADA8', 'E599B7', 'D93C51', '906A31', 'A642AD', '4893D8', '55DC80']
 
     let playersListArray = []
 
@@ -29,9 +30,14 @@ function app() {
     function addPlayerToBox(player) {
         const boxOfPlayers = document.querySelector('.boxes')
         const newBox = document.createElement('div')
-        newBox.style.backgroundColor = 'red'
+        newBox.style.backgroundColor = `#${getRandomColor()}`
         newBox.className = 'box'
         boxOfPlayers.appendChild(newBox).textContent = player
+    }
+
+    function getRandomColor() {
+        randomColor = Math.ceil(Math.random() * (colors.length))
+        return colors[randomColor]
     }
 
 
@@ -46,6 +52,8 @@ function app() {
     playButton.onclick = function() {
         console.log('A JUGAR')
     }
+
+
 }
 
 app()
