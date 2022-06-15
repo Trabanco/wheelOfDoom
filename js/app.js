c8bae938ab5a14bffdc7588a6bcb5c46767afef9
Mario Rubio <marioastur@gmail.com>
2022-06-15 15:52:28 +0200

    Añadimos automaticamente los seis priemros jugadores

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 function app() {
     const addButton = document.getElementById('add')
-    const editButton = document.getElementById('edit')
-    const deleteButton = document.getElementById('delete')
+    const editButton = document.querySelector(".edit")
+    const deleteButton = document.querySelector(".delete")
     const playButton = document.getElementById('play')
     const colors = ['D93C51', 'F6EB15', '4893D8', 'A642AD', '4C6BDB', '186887', 'DF3C54', 'ED8EF7', 'F6EB15', 'F98323', 'ED8EF7', '52ADA8', 'E599B7', 'D93C51', '906A31', 'A642AD', '4893D8', '55DC80']
-
+    const startPlayers = ['Bea', 'Susana', 'Mario', 'Buda', 'Max']
     let playersListArray = []
+
+
+    startPlayers.map(element => {
+        addPlayerToList(element)
+        addPlayerToBox(element)
+    });
+
 
     addButton.onclick = function() {
         const player = document.getElementById('input')
         if (player.value.trim() !== '') {
             addPlayerToList(player.value)
             addPlayerToBox(player.value)
+
         }
         player.value = ''
         player.focus()
     }
 
+
     function addPlayerToList(player) {
-        buttonsOnList = '<button type="button" class="buttonlist" id="edit">Edit</button><button type="button" class="buttonlist" id="delete">Delete</button>'
+        buttonsOnList = '<button type="button" class="buttonlist edit"><img src="./src/editButton.png"></button><button type="button" class="buttonlist delete"><img src="./src/deleteButton.png"></button>'
         let listPlayers = document.getElementById('orderlist')
         const newLine = document.createElement('div')
-        newLine.className = 'boxplay'
+        newLine.className = 'boxlist'
         player = `<p>${player}</p>${buttonsOnList}`
         listPlayers.appendChild(newLine).innerHTML = player
         playersListArray.push(player)
@@ -52,8 +61,6 @@ function app() {
     playButton.onclick = function() {
         console.log('A JUGAR')
     }
-
-
 }
 
 app()
